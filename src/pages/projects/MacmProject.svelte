ecdc03dcca335fd01af28f75547ed6aa6da4d6cf
Daniel MS <danielmccannsayles@gmail.com>
2025-02-21 21:08:55 -0800

    small updates

--- src/pages/projects/MacmProject.svelte
+++ src/pages/projects/MacmProject.svelte
@@ -42,7 +42,7 @@ They also try and give the LLMs roles, with a judge, executor, and ideator, but 
   />
 
   <MdCell
-    md="My goal was this project was just to learn, and mess around, using what they have as a baseline. Here is a rough timeline of what I attempted:
+    md="My goal with this project was just to learn, and mess around, using what they have as a baseline. Here is a rough timeline of what I attempted:
 
 1. First, I cloned and looked at the existing code base
     1. I re-wrote a lot of their stuff to be modifiable, and added helper functions.
@@ -199,7 +199,122 @@ async def agenerate_from_code_assistant(persona, content, assistant, thread):
   />
 
   <Section title="Code:" inset={true} hideToStart={true}>
-    <CodeCell code={``} />
+    <CodeCell
+      code={`# chains/main4/main.py
+
+async def main4(
+    question, max_times_mining_new, coding_assistant, coding_thread, log_path
+):
+
+    AsyncLogger.add_message(log_path, "## Begin")
+    CustomLogger.print(f"Starting {log_path}")
+
+    conditions, objectives = await extract_from_original(question)
+    c_str, o_str = conditions_objectives_to_string(conditions, objectives)
+    AsyncLogger.add_message(
+        log_path, "Extracted from problem, (conditions, objectives)", c_str, o_str
+    )
+
++    # New condition mining loop has a max # of tries
++    for i in range(max_times_mining_new):
+        unchecked_conditions = await new_conditions_from_existing(
+            conditions, objectives
+        )
+        AsyncLogger.add_message(
+            log_path,
+            f"## Mined new conditions from existing ({i+1}/{max_times_mining_new}):"
+            f"Unchecked Conditions ({i+1}/{max_times_mining_new})",
+            list_to_numbered_string(unchecked_conditions),
+        )
+
+        verified_conditions = []
+        # Check each new condition w/ the judge - check_statment
+        for i, unchecked in enumerate(unchecked_conditions):
+            AsyncLogger.add_message(log_path, f"Verifying condition #{i+1}")
+
++            # Verifies new condition. If it's wrong, tries to correct. Verifies that corrected condition. If wrong again, returns None
++            verified = await verify_new_condition(
+                objectives,
+                conditions,
+                unchecked,
+                coding_assistant,
+                coding_thread,
+            )
+            if verified:
+                verified_conditions.append(verified.new_condition)
+                AsyncLogger.add_message(log_path, "Verified", verified.new_condition)
+            else:
+                AsyncLogger.add_message(
+                    log_path,
+                    "Invalid",
+                )
+
++        # Add the new verified conditions to the exisiting conditions!
++        conditions += verified_conditions
++        AsyncLogger.add_message(
++            log_path, "All Conditions: ", list_to_numbered_string(conditions)
++        )
++
++        if_got_answer = await check_answer(conditions, objectives)
++        AsyncLogger.add_message(log_path, f"Do we have answer?: {if_got_answer}")
++
++        if if_got_answer:
++            break
+
+    CustomLogger.print(f"finished mining conditions ({log_path})")
+
+    # TODO: Not sure how to make this work yet.
+    # We should have a list of conditions that are ready. Let's check and see if any contradict
+    # This will return the conditions if they are good, and remove any that contradict
+    # conditions = double_check_conditions(conditions)
+
+    steps = await create_steps(conditions, objectives)
+
++    # Check if steps are accurate - 2 retries.
++    for _ in range(2):
++        accept_or_reject = await verify_steps(steps, objectives)
++        AsyncLogger.add_message(
++            log_path,
++            "### Step",
++            steps,
++        )
++        if accept_or_reject.acccept:
++            AsyncLogger.add_message(
++                log_path,
++                "Approved",
++            )
++            break
++        # Pass in the reason for failure
++        steps = await create_steps(
++            conditions,
++            objectives,
++            f"The last step generation process failed becase:\n {accept_or_reject.reason}",
++        )
+
+    AsyncLogger.add_message(
+        log_path,
+        "## Final",
+        "Conditions:",
+        list_to_numbered_string(conditions),
+        "Objectives:",
+        list_to_numbered_string(objectives),
+        "Steps:",
+        steps,
+    )
+    answer = await execute_steps(
+        conditions, objectives, steps, coding_assistant, coding_thread
+    )
+    AsyncLogger.add_message(log_path, "Generated Answer: ", answer)
+
+    # Flush out one message
+    await AsyncLogger.flush_one(log_path)
+
+    CustomLogger.print(
+        f"Finished {log_path} in ({CustomLogger.elapsed_time()}): the final answer is {answer}"
+    )
+    return answer`}
+      language="diff"
+    />
   </Section>
 
   <MdCell
