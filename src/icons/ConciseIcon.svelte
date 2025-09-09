47f9c091908f2a4948f8f349fc2be877dee143d3
Daniel MS <danielmccannsayles@gmail.com>
2025-09-08 21:23:31 -0700

    improve icons

--- src/icons/ConciseIcon.svelte
+++ src/icons/ConciseIcon.svelte
@@ -1,22 +1,29 @@
 <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
   <!-- ^ shape at top -->
   <path
-    d="M4 6 L8 2 L12 6"
+    d="M5 6 L8 3 L11 6"
     stroke="currentColor"
     stroke-width="1.5"
     fill="none"
     stroke-linecap="round"
     stroke-linejoin="round"
+    style="transition: 'd 0.3s ease'"
   />
   <!-- Vertical line in middle  -->
-  <rect x="6.5" y="6" width="2" height="8" />
+  <path
+    d="M8 6 L8 10"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+  />
   <!-- V shape at bottom -->
   <path
-    d="M4 10 L8 14 L12 10"
+    d="M5 10 L8 13 L11 10"
     stroke="currentColor"
     stroke-width="1.5"
     fill="none"
     stroke-linecap="round"
     stroke-linejoin="round"
+    style="transition: 'd 0.3s ease'"
   />
 </svg>
--- src/icons/VerboseIcon.svelte
+++ src/icons/VerboseIcon.svelte
@@ -1,22 +1,29 @@
 <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
   <!-- V shape at top  -->
   <path
-    d="M4 2 L8 6 L12 2"
+    d="M5 3 L8 6 L11 3"
     stroke="currentColor"
     stroke-width="1.5"
     fill="none"
     stroke-linecap="round"
     stroke-linejoin="round"
+    style="transition: 'd 0.3s ease' "
   />
   <!-- Vertical line in middle  -->
-  <rect x="6.5" y="6" width="2" height="8" />
+  <path
+    d="M8 6 L8 10"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+  />
   <!-- ^ shape at bottom  -->
   <path
-    d="M4 14 L8 10 L12 14"
+    d="M5 13 L8 10 L11 13"
     stroke="currentColor"
     stroke-width="1.5"
     fill="none"
     stroke-linecap="round"
     stroke-linejoin="round"
+    style="transition: 'd 0.3s ease'"
   />
 </svg>
